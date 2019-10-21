e7fac70d5541d803bc03179486d9649fb4f0f95d
alanamfanelli <56361850+alanamfanelli@users.noreply.github.com>
2019-10-21 11:07:44 -0600

    add Pokedex obeject array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
-alert('Hello world');
-var favoriteFood = 'sushi';
-document.write(favoriteFood);
+var repository = [ /* Pokedex Object Array*/
+  {
+      name:'Squirtle',
+      height:0.5,
+      type:['water']
+  },
+  {
+      name:'Butterfree',
+      height:1.1,
+      type:['bug','flying']
+  },
+  {
+      name:'Pikachu',
+      height:1.04,
+      type:['electric']
+  },
+  {
+      name:'Ninetales',
+      height:1.1,
+      type:['fire']
+  },
+];
